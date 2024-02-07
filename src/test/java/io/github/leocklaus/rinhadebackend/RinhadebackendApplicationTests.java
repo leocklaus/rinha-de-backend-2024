15f697a731359aff77b252c3b63575b1286740a4
leocklaus <leocklaus@gmail.com>
2024-02-07 16:51:29 -0300

    project created

---
+++ src/test/java/io/github/leocklaus/rinhadebackend/RinhadebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.rinhadebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhadebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
