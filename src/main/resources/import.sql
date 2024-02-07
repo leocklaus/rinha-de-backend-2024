304f12194aff64b371097853836066c57c007fdd
leocklaus <leocklaus@gmail.com>
2024-02-07 17:37:22 -0300

    feat: data seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO client (id, credit_limit, initial_balance) VALUES (1,100000,0);
+INSERT INTO client (id, credit_limit, initial_balance) VALUES (2,80000,0);
+INSERT INTO client (id, credit_limit, initial_balance) VALUES (3,1000000,0);
+INSERT INTO client (id, credit_limit, initial_balance) VALUES (4,10000000,0);
+INSERT INTO client (id, credit_limit, initial_balance) VALUES (5,500000,0);
