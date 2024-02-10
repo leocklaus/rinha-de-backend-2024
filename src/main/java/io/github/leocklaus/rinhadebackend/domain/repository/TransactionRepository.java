acf8c35420d3327016c169ad6197bfa4840220d5
leocklaus <leocklaus@gmail.com>
2024-02-10 11:24:33 -0300

    feat: new query to get client first 10 transactions ordered by creation date

--- src/main/java/io/github/leocklaus/rinhadebackend/domain/repository/TransactionRepository.java
+++ src/main/java/io/github/leocklaus/rinhadebackend/domain/repository/TransactionRepository.java
@@ -1,9 +1,14 @@
 package io.github.leocklaus.rinhadebackend.domain.repository;
 
+import io.github.leocklaus.rinhadebackend.domain.model.Client;
 import io.github.leocklaus.rinhadebackend.domain.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findFirst10ByClientOrderByCreatedAt(Client user
+    );
 }
