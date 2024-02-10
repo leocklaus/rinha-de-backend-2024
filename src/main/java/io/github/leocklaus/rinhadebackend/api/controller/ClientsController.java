8bd28d6116a732123c9ccdcea48115e3a5cc8ff7
leocklaus <leocklaus@gmail.com>
2024-02-10 11:23:37 -0300

    feat: getStatement method created

--- src/main/java/io/github/leocklaus/rinhadebackend/api/controller/ClientsController.java
+++ src/main/java/io/github/leocklaus/rinhadebackend/api/controller/ClientsController.java
@@ -1,6 +1,9 @@
 package io.github.leocklaus.rinhadebackend.api.controller;
 
+import io.github.leocklaus.rinhadebackend.domain.exception.ClientNotFound;
+import io.github.leocklaus.rinhadebackend.domain.exception.InsufficientLimit;
 import io.github.leocklaus.rinhadebackend.domain.model.Transaction;
+import io.github.leocklaus.rinhadebackend.domain.service.TransactionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,9 +11,36 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/clients")
 public class ClientsController {
 
+    private final TransactionService transactionService;
+
+    public ClientsController(TransactionService transactionService){
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping ("/{id}/extrato")
+    public ResponseEntity<?> getStatement(@PathVariable long userId){
+        try{
+            var statement = transactionService.getStatement(userId);
+            return ResponseEntity.ok(statement);
+        }catch (ClientNotFound clientNotFound){
+            ResponseEntity.notFound();
+        }finally {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
     @PostMapping("/{id}/transacoes")
     public ResponseEntity<?> createTransaction(@PathVariable long userId, @RequestBody Transaction transaction){
-        return ResponseEntity.ok().build();
+        try{
+            var transactionDTO = transactionService.crateTransaction(userId, transaction);
+            return ResponseEntity.ok(transactionDTO);
+        }catch (ClientNotFound clientNotFound){
+            ResponseEntity.notFound();
+        }catch (InsufficientLimit insufficientLimit){
+            ResponseEntity.unprocessableEntity();
+        }finally {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 }
